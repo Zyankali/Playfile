22b8a97c3cd78b93d510f481f9a1b6e57b75902f
Zyankali <silentsands@web.de>
2022-05-09 16:14:45 +0200

    Addes some more Routines. Need Balanceing

--- sysstarter.cpp
+++ sysstarter.cpp
@@ -55,4 +55,60 @@ void Startup::upstart()
 	Sleep(1000);
 	cout << "[ OK ] Summon daemon                " << "\t\n";
 
+	for (i = 0; i < 20000; i++)
+	{
+		cout << "[.   ] Network Device 001OIP.   " << "\t\r" << flush;
+		cout << "[..  ] Network Device 001OIP..  " << "\t\r" << flush;
+		cout << "[... ] Network Device 001OIP... " << "\t\r" << flush;
+		cout << "[....] Network Device 001OIP...." << "\t\r" << flush;
+	}
+
+	cout << "[ OK ] Network Device 001OIP                " << "\t\n";
+
+	for (i = 0; i < 50000; i++)
+	{
+		cout << "[.   ] Setting up External Devices.   " << "\t\r" << flush;
+		cout << "[..  ] Setting up External Devices..  " << "\t\r" << flush;
+		cout << "[... ] Setting up External Devices... " << "\t\r" << flush;
+		cout << "[....] Setting up External Devices...." << "\t\r" << flush;
+	}
+
+	cout << "[ OK ] Setting up External Devices                " << "\t\n";
+
+	for (i = 0; i < 50000; i++)
+	{
+		cout << "[.   ] FINAL startup and fastcheck.   " << "\t\r" << flush;
+		cout << "[..  ] FINAL startup and fastcheck..  " << "\t\r" << flush;
+		cout << "[... ] FINAL startup and fastcheck... " << "\t\r" << flush;
+		cout << "[....] FINAL startup and fastcheck...." << "\t\r" << flush;
+	}
+
+	cout << "[ OK ] FINAL startup and fastcheck                " << "\t\n";
+	sleep(2000);
+	cout << "All Done! Ready to go" << "\t\n";
+		cout << "[10]             " << "\t\r" << flush;
+		sleep(1000);
+
+		cout << "[09]            " << "\t\r" << flush;
+		sleep(1000);
+		cout << "[08]            " << "\t\r" << flush;
+		sleep(1000);
+		cout << "[07]            " << "\t\r" << flush;
+		sleep(1000);
+		cout << "[06]            " << "\t\r" << flush;
+		sleep(1000);
+		cout << "[05]            " << "\t\r" << flush;
+		sleep(1000);
+		cout << "[04]            " << "\t\r" << flush;
+		sleep(1000);
+		cout << "[03]            " << "\t\r" << flush;
+		sleep(1000);
+		cout << "[02]            " << "\t\r" << flush;
+		sleep(1000);
+		cout << "[01]            " << "\t\r" << flush;
+		sleep(1000);
+		cout << "[00] HELP! ARGL!" << "\t\r" << flush;
+		sleep(200);
+
+
 }
